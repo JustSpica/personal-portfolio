4fe2b3d0bb74fcc66abf7296bd73ea96bfb3a6b1
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-09-28 02:12:07 -0300

    feat(api/repos): created endpoint that returns filtered github profile repositories

---
+++ src/@types/github.d.ts
@@ -0,0 +1,10 @@
+export interface GithubRepo {
+  id: number
+  name: string
+  full_name: string
+  private: boolean
+  html_url: string
+  description: string
+  homepage: string
+  topics: string[]
+}
---
+++ src/app/api/repos/route.ts
@@ -0,0 +1,24 @@
+import { type NextRequest, NextResponse } from "next/server"
+
+import type { GithubRepo } from "@/@types/github"
+
+export async function GET(request: NextRequest) {
+  try {
+    const data = await fetch("https://api.github.com/users/JustSpica/repos", {
+      next: {
+        revalidate: 10,
+      },
+    })
+    const repos = (await data.json()) as GithubRepo[]
+
+    const topic = request.nextUrl.searchParams.get("topic")
+
+    const filteredReposByTopic = repos.filter((repo) => {
+      return repo.topics.includes(String(topic))
+    })
+
+    return NextResponse.json(filteredReposByTopic)
+  } catch (error) {
+    console.log(error)
+  }
+}
