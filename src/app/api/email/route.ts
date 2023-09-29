3c3627a775e65a43041aee77f8a9cfacfdcd0fd1
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-09-29 02:27:48 -0300

    feat(api/email): created endpoint that send email

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse, type NextRequest } from "next/server"
+import { Resend } from "resend"
+import * as zod from "zod"
+
+import ContactEmailTemplate from "@/emails/contact"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+const emailFormSchema = zod.object({
+  name: zod.string().min(1, { message: "Required" }),
+  email: zod.string().optional(),
+  subject: zod.string().min(1, { message: "Required" }),
+  message: zod.string().min(1, { message: "Required" }),
+})
+
+type EmailFormType = zod.infer<typeof emailFormSchema>
+
+interface ResendEmailsResponse {
+  id: string
+  message?: string
+  statusCode?: number
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const { message, name, subject, email } =
+      (await req.json()) as EmailFormType
+
+    const resendData = (await resend.emails.send({
+      from: `Personal <email@${String(process.env.RESEND_DOMAIN_EMAIL)}>`,
+      /* from: `${name} <onboarding@resend.dev>`, */
+      to: [String(process.env.RESEND_PERSONAL_EMAIL)],
+      subject: `Email de contato: ${subject}`,
+      react: ContactEmailTemplate({
+        message,
+        name,
+        subject,
+        email,
+      }),
+    })) as ResendEmailsResponse
+
+    if (resendData.statusCode && resendData.statusCode !== 200) {
+      throw new Error(resendData.message)
+    }
+
+    return NextResponse.json(resendData)
+  } catch (error) {
+    return NextResponse.json({ message: String(error) }, { status: 500 })
+  }
+}
